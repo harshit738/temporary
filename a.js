b9ea4dc6176a4a153e6ee4dbd3e78878a24e4d64
harshit738 <harshit.trehan12@gmail.com>
2021-04-09 19:19:22 +0530

    first_commit

---
+++ a.js
@@ -0,0 +1,7 @@
+console.log("Hello");
+
+// git --version  => to see the version
+// git init  => to initalize git in folder
+// git remote add origin (url).git  => to link the local folder with git repository
+// git add . => track the changes made in file
+// git status => tells the changes which are tracked
